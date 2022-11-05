f911f12373a3c4386b169b8d16913f6d127260a0
“Housam <housam.kak20@gmail.com>
2022-11-05 20:44:05 +0200

    frontend => create and export the Dob Page

--- frontend/views/Logging/DobPage.js
+++ frontend/views/Logging/DobPage.js
@@ -3,3 +3,7 @@ import DateTimePicker from "@react-native-community/datetimepicker";
 import { StyleSheet, View, Text, Image } from "react-native";
 import DateBtn from "../../components/DateBtn";
 import SignBtn from "../../components/SignBtn";
+
+const DobPage = () => { };
+
+export default DobPage;
