0f9a3231dd1aebebffc68b80718607728907a310
“Housam <housam.kak20@gmail.com>
2022-11-05 18:35:39 +0200

    frontend => structured the LogInPage

--- frontend/views/LoggingControl/LogInPage.js
+++ frontend/views/LoggingControl/LogInPage.js
@@ -1,2 +1,11 @@
 import * as React from "react";
 import { StyleSheet, View, Text, Image } from "react-native";
+const LogInPage = () => {
+  return (
+    <View style={styles.logInPageView}>
+      <Image></Image>
+      <View style={styles.signInContView}></View>
+      <View style={styles.signUpContView}></View>
+    </View>
+  );
+};
