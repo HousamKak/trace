a23cb9fcd7b8957ab05425f2050b2a80d7b3c2c4
“Housam <housam.kak20@gmail.com>
2022-11-05 18:47:14 +0200

    frontend => added splashscreen

--- frontend/App.js
+++ frontend/App.js
@@ -2,5 +2,12 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 const Stack = createNativeStackNavigator();
 import LogInPage from "./views/Logging/LogInPage";
+
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { View } from "react-native";
+
+const App = () => {
+  const [hideSplashScreen, setHideSplashScreen] = React.useState(true);
+  const SplashScreen = () => <View />;
+  
+};
