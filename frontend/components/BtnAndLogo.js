edfd14441c9e63ead332ea75e4bb34b859037450
“Housam <housam.kak20@gmail.com>
2022-11-05 19:37:19 +0200

    frontend => added BtnAndLogo Structure

---
+++ frontend/components/BtnAndLogo.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { Image, Text, TouchableOpacity, StyleSheet } from "react-native";
+
+const BtnAndLogo = (props) => {
+    const logo = props.logo;
+    return (<TouchableOpacity>
+        <Image></Image>
+    </TouchableOpacity>);
+}
