94d9d30f9bb4f300f472d0473d2b5e28449a0ae8
“Housam <housam.kak20@gmail.com>
2022-11-05 20:14:01 +0200

    frontend => structured the SignIn view

--- frontend/views/Logging/SignIn.js
+++ frontend/views/Logging/SignIn.js
@@ -3,6 +3,16 @@ import SignBtn from "../../components/SignBtn";
 // import InputField from "../../components/InputField";
 import { StyleSheet, View, Text, Image, TextInput } from "react-native";
 
-const SignIn = () => { return (""); };
+const SignIn = () => {
+    return
+    (<View>
+        <image />
+        <View>
+            <InputField></InputField>
+            <InputField></InputField>
+            <SignBtn></SignBtn>
+        </View>
+    </View>);
+};
 
 export default SignIn;
