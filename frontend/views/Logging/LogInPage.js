54ecce8b1c489a75150847d0fc5bbb9ddd88b285
“Housam <housam.kak20@gmail.com>
2022-11-05 19:34:49 +0200

    frontend => created SignBtn and BtnAndLogo files

--- frontend/views/Logging/LogInPage.js
+++ frontend/views/Logging/LogInPage.js
@@ -1,5 +1,8 @@
 import * as React from "react";
 import { StyleSheet, View, Text, Image } from "react-native";
+// import SignBtn from "../../components/SignBtn";
+// import BtnAndLogo from "../../components/BtnAndLogo";
+
 const LogInPage = () => {
   return (
     <View style={styles.logInPageView}>
