da9947a79d52b349b4914d0978cbf3429d100e09
“Housam <housam.kak20@gmail.com>
2022-11-05 19:52:36 +0200

    frontend => defined navigation inside the SignBtn

--- frontend/components/SignBtn.js
+++ frontend/components/SignBtn.js
@@ -2,5 +2,7 @@ import * as React from "react";
 import { Text, StyleSheet, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
-const SignBtn = (props) => { };
+const SignBtn = (props) => {
+    const navigation = useNavigation();
+};
 export default SignBtn;
