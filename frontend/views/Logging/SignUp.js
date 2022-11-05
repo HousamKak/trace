dc503339057b139e37ffbd7eff099c07bfa103d7
“Housam <housam.kak20@gmail.com>
2022-11-05 20:27:20 +0200

    frontend => added logo properties

--- frontend/views/Logging/SignUp.js
+++ frontend/views/Logging/SignUp.js
@@ -8,7 +8,10 @@ const SignIn = () => {
 
     return (
         <View style={styles.signInView}>
-            <Image />
+            <Image
+                style={styles.logoIcon}
+                resizeMode="cover"
+                source={require("../../assets/LogInPage/logo.png")} />
             <View style={styles.rectangleView}>
                 <InputField placeholder="Username"></InputField>
                 <InputField placeholder="Email"></InputField>
