4968b92538b6c48870f4221ba6df02c24d7688b3
“Housam <housam.kak20@gmail.com>
2022-11-05 20:12:06 +0200

    frontend => removed spacings

--- frontend/components/InputField.js
+++ frontend/components/InputField.js
@@ -2,35 +2,35 @@ import * as React from "react";
 import { Text, StyleSheet, TextInput, View } from "react-native";
 
 const InputField = (props) => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.text}>{props.placeholder}</Text>
-            <TextInput
-                style={styles.field}
-                placeholder={props.placeholder}
-                mode="flat"
-                placeholderTextColor="#818181"
-            />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>{props.placeholder}</Text>
+      <TextInput
+        style={styles.field}
+        placeholder={props.placeholder}
+        mode="flat"
+        placeholderTextColor="#818181"
+      />
+    </View>
+  );
 };
-
 const styles = StyleSheet.create({
-    field: {
-        height: 55,
-        width: 331,
-        borderRadius: 12,
-        backgroundColor: "#fff",
-        paddingHorizontal: 8,
-    },
-    container: {
-        flexDirection: "column",
-    },
-    text: {
-        fontSize: 16,
-        color: "#fff",
-        textAlign: "left",
-        marginBottom: 6,
-    },
+  field: {
+    height: 55,
+    width: 331,
+    borderRadius: 12,
+    backgroundColor: "#fff",
+    paddingHorizontal: 8,
+  },
+  container: {
+    flexDirection: "column",
+  },
+  text: {
+    fontSize: 16,
+    color: "#fff",
+    textAlign: "left",
+    marginBottom: 6,
+  },
 });
+
 export default InputField;
