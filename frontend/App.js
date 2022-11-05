5b8bcfaa74f7eea492a925692a249352793ddd07
“Housam <housam.kak20@gmail.com>
2022-11-05 18:31:03 +0200

    frontend => imported needed libraries to App.js

---
+++ frontend/App.js
@@ -0,0 +1,4 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+const Stack = createNativeStackNavigator();
+import LogInPage from "./views/Logging/LogInPage";
