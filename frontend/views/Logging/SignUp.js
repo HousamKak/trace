275f530658e5a82bc248e26a3e690a6ea960d0bd
“Housam <housam.kak20@gmail.com>
2022-11-05 20:23:32 +0200

    frontend => imported needed libraries

---
+++ frontend/views/Logging/SignUp.js
@@ -0,0 +1,4 @@
+import * as React from "react";
+import SignBtn from "../../components/SignBtn";
+import InputField from "../../components/InputField";
+import { StyleSheet, View, Image } from "react-native";
