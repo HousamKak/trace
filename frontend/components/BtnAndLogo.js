e0c417a34e68bf91c0e00c4e2d186669b317ab0b
“Housam <housam.kak20@gmail.com>
2022-11-05 19:44:01 +0200

    frontend => removed unused variable logo

--- frontend/components/BtnAndLogo.js
+++ frontend/components/BtnAndLogo.js
@@ -2,7 +2,6 @@ import * as React from "react";
 import { Image, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 const BtnAndLogo = (props) => {
-    const logo = props.logo;
     return (
         <TouchableOpacity
             style={[styles.btnAndLogoTouchableOpacity, props.bstyle]}
