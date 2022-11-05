1a986649030d2699ef25947ec3ac57ddd206ba12
“Housam <housam.kak20@gmail.com>
2022-11-05 20:17:55 +0200

    frontend => added imported input field component

--- frontend/views/Logging/SignIn.js
+++ frontend/views/Logging/SignIn.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import SignBtn from "../../components/SignBtn";
-// import InputField from "../../components/InputField";
+import InputField from "../../components/InputField";
 import { StyleSheet, View, Text, Image, TextInput } from "react-native";
 
 const SignIn = () => {
