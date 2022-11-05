84c9c1ae741764f3870add1f050620049276539a
“Housam <housam.kak20@gmail.com>
2022-11-05 19:53:13 +0200

    frontend => structured the Btn

--- frontend/components/SignBtn.js
+++ frontend/components/SignBtn.js
@@ -4,5 +4,14 @@ import { useNavigation } from "@react-navigation/native";
 
 const SignBtn = (props) => {
     const navigation = useNavigation();
+    return (
+        <TouchableOpacity
+            style={[styles.signBtnTouchableOpacity, props.cwidth]}
+            activeOpacity={0.2}
+            onPress={() => navigation.navigate(props.navigation)}
+        >
+            <Text style={styles.signText}> {props.text}</Text>
+        </TouchableOpacity>
+    );
 };
 export default SignBtn;
