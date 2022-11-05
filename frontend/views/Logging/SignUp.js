de777c1dc4699896605c4b3c05ab499f6a6d2e13
“Housam <housam.kak20@gmail.com>
2022-11-05 20:24:06 +0200

    frontend => created and exported SignIn function

--- frontend/views/Logging/SignUp.js
+++ frontend/views/Logging/SignUp.js
@@ -2,3 +2,9 @@ import * as React from "react";
 import SignBtn from "../../components/SignBtn";
 import InputField from "../../components/InputField";
 import { StyleSheet, View, Image } from "react-native";
+
+
+const SignIn = () => { };
+
+
+export default SignIn;
