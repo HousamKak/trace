ea471d8d00a241a416521a19bddc8495869ff9c4
“Housam <housam.kak20@gmail.com>
2022-11-05 18:46:10 +0200

    frontend => imported navigation to App.js

--- frontend/App.js
+++ frontend/App.js
@@ -2,4 +2,5 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 const Stack = createNativeStackNavigator();
 import LogInPage from "./views/Logging/LogInPage";
-
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View } from "react-native";
