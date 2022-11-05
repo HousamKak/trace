2e77f80ac7ac9ed673d5e62be81194048919f52d
“Housam <housam.kak20@gmail.com>
2022-11-05 20:09:05 +0200

    frontend => structured the InputField component

--- frontend/components/InputField.js
+++ frontend/components/InputField.js
@@ -1,6 +1,18 @@
 import * as React from "react";
 import { Text, StyleSheet, TextInput, View } from "react-native";
 
-const InputField = (props) => { };
+const InputField = (props) => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>{props.placeholder}</Text>
+            <TextInput
+                style={styles.field}
+                placeholder={props.placeholder}
+                mode="flat"
+                placeholderTextColor="#818181"
+            />
+        </View>
+    );
+};
 
 export default InputField;
