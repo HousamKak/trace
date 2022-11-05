72726e2685c7a6aae6d22df865b3d22c8b460b8f
“Housam <housam.kak20@gmail.com>
2022-11-05 20:38:48 +0200

    frontend => added the months array

--- frontend/components/DateBtn.js
+++ frontend/components/DateBtn.js
@@ -1,6 +1,21 @@
 import * as React from "react";
 import { Text, StyleSheet, TouchableOpacity } from "react-native";
 
-const DateBtn = (props) => { };
+const DateBtn = (props) => {
+    const monthNames = [
+        "Jan",
+        "Feb",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+    ];
+};
 
 export default DateBtn;
