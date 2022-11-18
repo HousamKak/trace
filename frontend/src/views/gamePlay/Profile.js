1ec78afc42c9084a36189a264208e937c7decccf
“Housam <housam.kak20@gmail.com>
2022-11-19 00:57:58 +0200

    frontend => added needed dependencies for profile page view

---
+++ frontend/src/views/gamePlay/Profile.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import {
+    Text,
+    StyleSheet,
+    View,
+    ScrollView, RefreshControl,
+    Image
+} from "react-native";
+import { useNavigation, } from "@react-navigation/native";
+import MenuBtn from "../../components/ButtonsMenu/MenuBtn";
+import SectionBar from "../../components/Breakers/SectionBar";
+import DataDisplay from "../../components/Displayers/DataDisplay";
