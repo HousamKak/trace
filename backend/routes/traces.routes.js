1897eb3f9c932eb92137903cfb3229bfcd4c5b34
“Housam <housam.kak20@gmail.com>
2022-11-18 14:11:09 +0200

    backend => reordered the routing requests

--- backend/routes/traces.routes.js
+++ backend/routes/traces.routes.js
@@ -2,10 +2,10 @@ const { Router } = require('express');
 const router = Router();
 const { getAllUserTraces, getTrace, deleteTrace, addTrace, getCloseTraces } = require('../controllers/traces.controller');
 
+router.get('/', getCloseTraces);
 router.get('/user/:user_id', getAllUserTraces);
-router.delete('/', deleteTrace);
 router.get('/:trace_id', getTrace);
 router.post('/', addTrace);
-router.get('/', getCloseTraces);
+router.delete('/', deleteTrace);
 
 module.exports = router;
