5538b56ff096767daf58d3de0f1f53aba1faa300
“Housam <housam.kak20@gmail.com>
2022-11-18 12:38:20 +0200

    backend => listened to app

--- backend/index.js
+++ backend/index.js
@@ -10,3 +10,9 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
+
+
+app.listen(process.env.PORT, (err) => {
+    if (err) { console.log(err); }
+    console.log(`server running on port ${process.env.PORT}`);
+});
