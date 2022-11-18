0c9982029abd1c56fbc2ddea402a82393e2e1946
“Housam <housam.kak20@gmail.com>
2022-11-18 21:43:05 +0200

    frontend => structed the DataDisplay component

--- frontend/components/Displayers/DataDisplay.js
+++ frontend/components/Displayers/DataDisplay.js
@@ -1,5 +1,14 @@
 import * as React from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
-const DataDisplay = (props) => { }
+const DataDisplay = (props) => {
+    return (
+        <View >
+            <View >
+                <Image />
+                <Text ></Text>
+            </View>
+            <Text></Text>
+        </View >)
+}
 
 export default DataDisplay;
