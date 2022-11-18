71f242398a319ad51562a2aea07c8e1a1257bafe
“Housam <housam.kak20@gmail.com>
2022-11-18 13:59:00 +0200

    backend => routed the getAllUserTraces by get request

--- backend/routes/traces.routes.js
+++ backend/routes/traces.routes.js
@@ -2,4 +2,6 @@ const { Router } = require('express');
 const router = Router();
 const { getAllUserTraces } = require('../controllers/traces.controller');
 
+router.get('/user/:user_id', getAllUserTraces);
+
 module.exports = router;
