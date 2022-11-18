185814aecca9f7d386df3faff6364fe626fe958d
“Housam <housam.kak20@gmail.com>
2022-11-18 13:17:07 +0200

    backend => got the token from headers

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,6 +1,8 @@
 const db = require('../config/db.config');
 const jwt = require('jsonwebtoken');
 
-const authMiddleware = async (req, res, next) => { }
+const authMiddleware = async (req, res, next) => {
+    const token = req.headers.authorization;
+}
 
 module.exports = authMiddleware;
