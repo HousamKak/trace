959947c43c926a3357c2b875661ae2e74e5fad42
“Housam <housam.kak20@gmail.com>
2022-11-18 13:49:35 +0200

    backend => implemented getAllSaves

--- backend/controllers/saves.controller.js
+++ backend/controllers/saves.controller.js
@@ -1,6 +1,14 @@
 const db = require('../config/db.config')
 
-const getAllSaves = (req, res) => { }
+const getAllSaves = (req, res) => {
+    const { user_id } = req.params;
+    if (user_id) {
+        db.query('SELECT * FROM traces WHERE trace_id IN (select saved_trace_id from saves where user_id = ?);', [user_id], (err, rows) => {
+            if (err) console.log(err);
+            res.json(rows);
+        })
+    }
+}
 
 const deleteSave = (req, res) => { }
 
