aedfcf5531170581b6cf138753492ac9bdc65fbf
“Housam <housam.kak20@gmail.com>
2022-11-18 12:44:43 +0200

    backend => handled unfound emails

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -5,6 +5,12 @@ const jwt = require("jsonwebtoken");
 
 const login = (req, res) => {
     const { email, password } = req.body;
-    db.query('SELECT * FROM users where email = ?', [email], (err, rows) => { });
+    db.query('SELECT * FROM users where email = ?', [email], (err, rows) => {
+        if (err) console.log(err);
+        if (rows[0]) { }
+        else {
+            res.json({ message: "Not Registered" })
+        }
+    });
 
 };
