6d7dfd4127964c85a89a2a5eb4db1a400a01f26e
“Housam <housam.kak20@gmail.com>
2022-11-18 21:35:02 +0200

    frontend => structured the MenuBtnText component

--- frontend/components/ButtonsMenu/MenuBtnText.js
+++ frontend/components/ButtonsMenu/MenuBtnText.js
@@ -2,6 +2,12 @@ import * as React from "react";
 import { StyleSheet, View, Text } from "react-native";
 import MenuBtn from "./MenuBtn";
 
-const MenuBtnText = (props) => { }
+const MenuBtnText = (props) => {
+    return (
+        <View style={styles.rectangleView}>
+            <Text style={[styles.text, props.edits]}>{props.text}</Text>
+            <MenuBtn src={props.src} backgroundColor={props.backgroundColor} onPress={props.onPress} />
+        </View>);
+}
 
 export default MenuBtnText;
