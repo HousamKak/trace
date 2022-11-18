744be708f6bf4a1aa8ccc91f42cbf396d5187e30
“Housam <housam.kak20@gmail.com>
2022-11-18 22:32:14 +0200

    frontend => imported validators from validate file

--- frontend/src/views/Logging/SignUp.js
+++ frontend/src/views/Logging/SignUp.js
@@ -3,6 +3,7 @@ import SignBtn from "../../components/ButtonsLogging/SignBtn";
 import InputField from "../../components/InputField";
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { StyleSheet, ScrollView, View, Image, Text } from "react-native";
+import { validateEmail, validatePassword } from "../../utilities/validate";
 
 const SignUp = () => {
     return (
