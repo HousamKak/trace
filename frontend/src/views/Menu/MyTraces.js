9b6d5542ef1201c49102982ec58e1027483d8c25
“Housam <housam.kak20@gmail.com>
2022-11-18 23:44:48 +0200

    frontend => added needed dependencies to myTraces

---
+++ frontend/src/views/Menu/MyTraces.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import {
+    Text,
+    StyleSheet,
+    View,
+    ScrollView, RefreshControl
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import FullCard from "../../components/Cards/FullCard";
+import MenuBtn from "../../components/ButtonsMenu/MenuBtn";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
