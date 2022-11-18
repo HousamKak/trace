b7ccaa6b053bc2c3c164118ddd72f05210676151
“Housam <housam.kak20@gmail.com>
2022-11-18 21:49:00 +0200

    frontend => defined regex for email checking

--- frontend/utilities/validate.js
+++ frontend/utilities/validate.js
@@ -1,4 +1,6 @@
-const validateEmail = (email) => { }
+const validateEmail = (email) => { 
+    const re = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,63})$/;
+}
 const validatePassword = (password) => { }
 
 export { validateEmail, validatePassword };
