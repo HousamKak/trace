1d50b7aac9373192630c642f60175e343fbe7730
“Housam <housam.kak20@gmail.com>
2022-11-18 14:01:52 +0200

    backend => routed the deleteTrace API by delete request

--- backend/routes/traces.routes.js
+++ backend/routes/traces.routes.js
@@ -3,5 +3,6 @@ const router = Router();
 const { getAllUserTraces,deleteTrace } = require('../controllers/traces.controller');
 
 router.get('/user/:user_id', getAllUserTraces);
+router.delete('/', deleteTrace);
 
 module.exports = router;
