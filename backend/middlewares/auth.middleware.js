6dba4221ae880d1a0a42670cd72fdbf6f1307cf9
“Housam <housam.kak20@gmail.com>
2022-11-18 13:18:32 +0200

    backend => added query to find user from token and finished Middleware

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -5,6 +5,13 @@ const authMiddleware = async (req, res, next) => {
     const token = req.headers.authorization;
     if (token) {
         const decoded = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET_KEY);
+        db.query('SELECT user_id FROM users where email = ?', [decoded.email], (err, rows) => {
+            if (err) console.log(err);
+            if (rows[0]) {
+                req.user = rows[0];
+                next();
+            }
+        })
     }
     else { res.json({ message: "Unauthorized" }) }
 }
