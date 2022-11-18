c213c419cd132147e178ea6d07e3488b718a19f0
“Housam <housam.kak20@gmail.com>
2022-11-18 12:37:35 +0200

    backend => configured the initialized app by express and bodyParser

--- backend/index.js
+++ backend/index.js
@@ -4,3 +4,9 @@ const bodyParser = require('body-parser');
 
 require('dotenv').config();
 const app = express();
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
