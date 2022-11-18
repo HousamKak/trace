727cab634c2bb813c1221b53de297fdced3c44f1
“Housam <housam.kak20@gmail.com>
2022-11-18 13:57:16 +0200

    backend => implemented getAllUserTraces API

--- backend/controllers/traces.controller.js
+++ backend/controllers/traces.controller.js
@@ -1,5 +1,17 @@
 const db = require("../config/db.config");
 
-const getAllUserTraces = (req, res) => { }
+const getAllUserTraces = (req, res) => {
+    const { user_id } = req.params;
+    if (user_id) {
+        db.query(
+            "SELECT * FROM traces WHERE user_id = ?",
+            [user_id],
+            (err, rows) => {
+                if (err) console.log(err);
+                res.send(rows);
+            }
+        );
+    }
+}
 
 module.exports = { getAllUserTraces };
