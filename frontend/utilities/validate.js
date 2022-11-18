582c6a54757a9ba7a5b384d87348eb594d7629ed
“Housam <housam.kak20@gmail.com>
2022-11-18 21:49:23 +0200

    frontend => checked for empty email

--- frontend/utilities/validate.js
+++ frontend/utilities/validate.js
@@ -1,5 +1,6 @@
 const validateEmail = (email) => { 
     const re = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,63})$/;
+    if (email === "") return { status: false, message: "Missing field" }
 }
 const validatePassword = (password) => { }
 
