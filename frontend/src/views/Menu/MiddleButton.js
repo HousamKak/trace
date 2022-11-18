71cbb00402848326190745fd071f44d225f59f5b
“Housam <housam.kak20@gmail.com>
2022-11-18 23:54:52 +0200

    frontend => defined the MiddleButton function

--- frontend/src/views/Menu/MiddleButton.js
+++ frontend/src/views/Menu/MiddleButton.js
@@ -11,3 +11,6 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import { useNavigation } from "@react-navigation/native";
 import axios from "axios";
 const base_url = "http://192.168.1.102:8000"
+
+
+const MiddleButton = () => { }
