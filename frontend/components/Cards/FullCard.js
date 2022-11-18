a6edad8ed526db3d15b89f04edb2d53de0b22419
“Housam <housam.kak20@gmail.com>
2022-11-18 21:39:15 +0200

    frontned => added a button to FullCard

--- frontend/components/Cards/FullCard.js
+++ frontend/components/Cards/FullCard.js
@@ -2,6 +2,15 @@ import * as React from "react";
 import { Text, StyleSheet, TouchableOpacity, Image, View } from "react-native";
 
 
-const FullCard = (props) => { }
+const FullCard = (props) => {
+    return (
+        <TouchableOpacity
+            style={styles.rectangleView}
+            activeOpacity={0.2}
+        >
+
+        </TouchableOpacity >
+    );
+}
 
 export default FullCard;
