b78c4383f59b1c7dad2676a9cc442ebfe2082ac4
“Housam <housam.kak20@gmail.com>
2022-11-18 23:34:07 +0200

    frontend => added the Saved function

--- frontend/src/views/Menu/Saved.js
+++ frontend/src/views/Menu/Saved.js
@@ -1,11 +1,15 @@
 import * as React from "react";
 import {
-  Text,
-  StyleSheet,
-  View,
-  ScrollView, RefreshControl
+    Text,
+    StyleSheet,
+    View,
+    ScrollView, RefreshControl
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import FullCard from "../../components/Cards/FullCard";
 import MenuBtn from "../../components/ButtonsMenu/MenuBtn";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const Saved = () => { 
+    
+}
