84170638028d436641702b7d67e7ae732131fa37
“Housam <housam.kak20@gmail.com>
2022-11-18 13:33:49 +0200

    backend => define the APis routes

--- backend/routes/friends.routes.js
+++ backend/routes/friends.routes.js
@@ -2,3 +2,7 @@ const { Router } = require('express');
 const router = Router();
 
 const { getAllFriends, addFriend, deleteFriend } = require('../controllers/friends.controller');
+
+router.get('/:user_id', getAllFriends);
+router.post('/', addFriend);
+router.delete('/', deleteFriend);
