5f8469bd6242190c0d9114e6e2ccf33c672cd728
“Housam <housam.kak20@gmail.com>
2022-11-18 23:47:16 +0200

    frontend => added refreshing feature

--- frontend/src/views/Menu/MyTraces.js
+++ frontend/src/views/Menu/MyTraces.js
@@ -13,4 +13,13 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 const MyTraces = () => {
     const navigation = useNavigation();
     const [myTrace, setMyTrace] = React.useState([]);
+
+    const [refreshing, setRefreshing] = React.useState(false);
+    const wait = (timeout) => {
+        return new Promise(resolve => setTimeout(resolve, timeout));
+    }
+    const onRefresh = React.useCallback(() => {
+        setRefreshing(true);
+        wait(500).then(() => setRefreshing(false));
+    }, []);
 }
