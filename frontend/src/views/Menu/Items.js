65f67a700e86fc8114c8e57ab84ff5fad579ee1f
“Housam <housam.kak20@gmail.com>
2022-11-18 23:13:51 +0200

    frontend => added refreshing feature to Items

--- frontend/src/views/Menu/Items.js
+++ frontend/src/views/Menu/Items.js
@@ -14,4 +14,13 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 const Items = () => {
     const [myitems, setMyItems] = React.useState([])
     const navigation = useNavigation();
+
+    const [refreshing, setRefreshing] = React.useState(false);
+    const wait = (timeout) => {
+        return new Promise(resolve => setTimeout(resolve, timeout));
+    }
+    const onRefresh = React.useCallback(() => {
+        setRefreshing(true);
+        wait(500).then(() => setRefreshing(false));
+    }, []);
 }
