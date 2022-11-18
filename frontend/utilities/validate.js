663e9ac526bf2c1560a35c7a58125185a84551cd
“Housam <housam.kak20@gmail.com>
2022-11-18 21:49:47 +0200

    frontend => tested email against regex entry

--- frontend/utilities/validate.js
+++ frontend/utilities/validate.js
@@ -1,6 +1,9 @@
 const validateEmail = (email) => { 
     const re = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,63})$/;
     if (email === "") return { status: false, message: "Missing field" }
+    if (!re.test(email)) {
+        return { status: false, message: " Invalid Email" }
+    };
 }
 const validatePassword = (password) => { }
 
