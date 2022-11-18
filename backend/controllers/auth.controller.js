8c251380eb830c7d845bdd7be0f38985c6942630
“Housam <housam.kak20@gmail.com>
2022-11-18 12:43:13 +0200

    backend => added sql command to check for email

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,3 +1,10 @@
 const db = require('../config/db.config')
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+
+
+const login = (req, res) => {
+    const { email, password } = req.body;
+    db.query('SELECT * FROM users where email = ?', [email], (err, rows) => { });
+
+};
