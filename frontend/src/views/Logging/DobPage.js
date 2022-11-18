d1c4a5ec6549bc1e2b4118ff86fcb6a3b4917fb0
“Housam <housam.kak20@gmail.com>
2022-11-18 22:00:25 +0200

    frontend => imported AsyncStorage library to DobPage

--- frontend/src/views/Logging/DobPage.js
+++ frontend/src/views/Logging/DobPage.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import DateTimePicker from "@react-native-community/datetimepicker";
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import { StyleSheet, View, Text, Image } from "react-native";
 import DateBtn from "../../components/DateBtn";
 import SignBtn from "../../components/SignBtn";
