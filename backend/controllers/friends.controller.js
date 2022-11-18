96724f3bf1df205e24fd1a1f8656764729c3ed9a
“Housam <housam.kak20@gmail.com>
2022-11-18 13:21:40 +0200

    backend => Implemented getAllFriends API

--- backend/controllers/friends.controller.js
+++ backend/controllers/friends.controller.js
@@ -1,5 +1,13 @@
 const db = require('../config/db.config')
-const getAllFriends = (req, res) => { }
+const getAllFriends = (req, res) => {
+    const { user_id } = req.params;
+    if (user_id) {
+        db.query('SELECT * FROM users WHERE user_id IN(select friend_user_id from friends where user_id = ?)', [user_id], (err, rows) => {
+            if (err) console.log(err);
+            res.send(rows);
+        })
+    }
+}
 
 const addFriend = (req, res) => { }
 
