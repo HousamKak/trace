081260ab3b84bfcd45032ca837febb7a7b2e4161
“Housam <housam.kak20@gmail.com>
2022-11-18 12:46:47 +0200

    backend => generated token on success

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,18 +2,24 @@ const db = require('../config/db.config')
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-
 const login = (req, res) => {
     const { email, password } = req.body;
     db.query('SELECT * FROM users where email = ?', [email], (err, rows) => {
         if (err) console.log(err);
         if (rows[0]) {
             const user = rows[0];
-            bcrypt.compare(password, user.password, (err, isValid) => { });
+            bcrypt.compare(password, user.password, (err, isValid) => {
+                if (err) console.log(err);
+                if (isValid) {
+                    const token = jwt.sign({ email: user.email, name: user.username }, process.env.JWT_SECRET_KEY);
+                    res.status(200).json({ token, user, message: "Login Successful" });
+                } else {
+                    res.json({ message: "Invalid Credentials" });
+                }
+            });
         }
         else {
             res.json({ message: "Not Registered" })
         }
     });
-
 };
