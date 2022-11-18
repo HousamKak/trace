070877c9013810455f13a9ed8a420e63d9598535
“Housam <housam.kak20@gmail.com>
2022-11-18 22:46:29 +0200

    frontend => removed navigation prop from SignBtn

--- frontend/src/views/Logging/SignUp.js
+++ frontend/src/views/Logging/SignUp.js
@@ -67,7 +67,7 @@ const SignUp = () => {
                     {(errorEmail !== "") ? <Text style={styles.error_message}>{errorEmail}</Text> : ""}
                     <InputField placeholder="Password" onChange={setPassword}></InputField>
                     {(errorPassword !== "") ? <Text style={styles.error_message}>{errorPassword}</Text> : ""}
-                    <SignBtn navigation="" text="SIGN UP" cwidth={styles.sign} />
+                    <SignBtn  text="SIGN UP" cwidth={styles.sign} />
                 </View>
             </ScrollView>
         );
