52b8244f21f6f6ae020a6d78f1bab857b6a03c6c
“Housam <housam.kak20@gmail.com>
2022-11-19 00:56:28 +0200

    frontend => added needed dependencies for the MainPage

---
+++ frontend/src/views/gamePlay/MainPage.js
@@ -0,0 +1,9 @@
+// Here we should have unity page linked, but for the test version I will make a button in the middle of the screen
+
+import * as React from "react";
+import {
+    StyleSheet,
+    View,
+} from "react-native";
+import MenuBtn from "../../components/ButtonsMenu/MenuBtn";
+import { useNavigation } from "@react-navigation/native";
