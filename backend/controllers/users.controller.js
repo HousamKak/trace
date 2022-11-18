fb3699937b72382dfa99fd1863779eaa5c0ed9da
“Housam <housam.kak20@gmail.com>
2022-11-18 13:11:35 +0200

    backend =>  Implemented getUser

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -1,7 +1,15 @@
 const db = require('../config/db.config');
 const bcrypt = require("bcrypt");
 
-const getUser = (req, res) => { }
+const getUser = (req, res) => {
+    const { user_id } = req.user;
+    if (user_id) {
+        db.query('SELECT * FROM users where user_id = ?', [user_id], (err, rows) => {
+            if (err) console.log(err);
+            res.status(200).json(rows);
+        })
+    }
+}
 
 const updateUser = async (req, res) => { }
 
