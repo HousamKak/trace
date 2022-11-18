bfd27aac40caa1e66c7c32cbe66ec9edd4e960de
“Housam <housam.kak20@gmail.com>
2022-11-18 21:09:56 +0200

    frontend => structured the Section Bar and added props

--- frontend/components/Breakers/SectionBar.js
+++ frontend/components/Breakers/SectionBar.js
@@ -1,6 +1,14 @@
 import * as React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-const SectionBar = (props) => { }
+const SectionBar = (props) => { 
+    return (
+        <View style={styles.sectionBar}>
+            <View style={styles.sectionLine} />
+            <Text style={styles.sectionBarText}>{props.text}</Text>
+            <View style={styles.sectionLine} />
+        </View>
+    );
+}
 
 export default SectionBar;
