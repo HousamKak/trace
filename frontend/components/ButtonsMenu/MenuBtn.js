a166120b8411e6d23d32a15dd106f3849b6937b9
“Housam <housam.kak20@gmail.com>
2022-11-18 21:15:21 +0200

    frontend => structured the Menu Btn component with styling and props

--- frontend/components/ButtonsMenu/MenuBtn.js
+++ frontend/components/ButtonsMenu/MenuBtn.js
@@ -2,6 +2,17 @@ import * as React from "react";
 import { StyleSheet, TouchableOpacity, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
-const MenuBtn = (props) => { }
+const MenuBtn = (props) => {
+    return (
+        <TouchableOpacity
+            style={[styles.rectangleView, props.backgroundColor]}
+            activeOpacity={0.2}
+            onPress={props.onPress}>
+            <Image style={props.size}
+                resizeMode="cover"
+                source={props.src}
+            />
+        </TouchableOpacity>);
+}
 
 export default MenuBtn;
