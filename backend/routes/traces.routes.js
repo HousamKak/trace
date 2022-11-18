69e4b9f2846a1a435bf76cee6ec8dca69dab327f
“Housam <housam.kak20@gmail.com>
2022-11-18 14:03:54 +0200

    backend => imported the getTrace API into the routing file

--- backend/routes/traces.routes.js
+++ backend/routes/traces.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const router = Router();
-const { getAllUserTraces,deleteTrace } = require('../controllers/traces.controller');
+const { getAllUserTraces, getTrace, deleteTrace } = require('../controllers/traces.controller');
 
 router.get('/user/:user_id', getAllUserTraces);
 router.delete('/', deleteTrace);
