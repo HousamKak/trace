82250cee3737b3ed3ba2930e7e6cd5d8eeccde34
“Housam <housam.kak20@gmail.com>
2022-11-18 13:19:16 +0200

    backend => added authMiddleware to user routes

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const router = Router();
 const { getUser, updateUser, deleteUser } = require('../controllers/users.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
 
-router.get('/', getUser);
+router.get('/', authMiddleware, getUser);
 router.put('/', updateUser);
 router.delete('/', deleteUser);
 
