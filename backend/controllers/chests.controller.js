a0df52fb671da7a98e3b8ca07a2af499e3ceefea
“Housam <housam.kak20@gmail.com>
2022-11-18 13:42:02 +0200

    backend => implement the getChest API

--- backend/controllers/chests.controller.js
+++ backend/controllers/chests.controller.js
@@ -10,6 +10,18 @@ const getCloseChests = (req, res) => {
     );
 }
 
-const getChest = (req, res) => { }
+const getChest = (req, res) => {
+    const { chest_id } = req.params;
+    if (chest_id) {
+        db.query(
+            "SELECT * FROM chests WHERE chest_id = ?",
+            [chest_id],
+            (err, rows) => {
+                if (err) console.log(err);
+                res.send(rows);
+            }
+        );
+    }
+}
 
 module.exports = { getChest, getCloseChests };
