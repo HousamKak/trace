24dcb0d93a771c8bc1e660fd198ad99c59d110af
“Housam <housam.kak20@gmail.com>
2022-11-18 21:43:33 +0200

    frontend => added styles and props to tags in DataDisplay

--- frontend/components/Displayers/DataDisplay.js
+++ frontend/components/Displayers/DataDisplay.js
@@ -2,13 +2,13 @@ import * as React from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
 const DataDisplay = (props) => {
     return (
-        <View >
-            <View >
-                <Image />
-                <Text ></Text>
+        <View style={styles.DataDisplay}>
+            <View style={styles.query}>
+                <Image style={props.cheat} source={props.src} />
+                <Text style={styles.data}>{props.data}</Text>
             </View>
-            <Text></Text>
-        </View >)
+            <Text style={styles.info}>{props.info}</Text>
+        </View>)
 }
 
 export default DataDisplay;
