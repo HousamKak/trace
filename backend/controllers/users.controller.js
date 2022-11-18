7a7f660407cd67d57d527092750774c87a5ecf72
“Housam <housam.kak20@gmail.com>
2022-11-18 13:11:00 +0200

    backend => defined and exported getUser, UpdateUser, and deleteUser

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -1,2 +1,10 @@
 const db = require('../config/db.config');
 const bcrypt = require("bcrypt");
+
+const getUser = (req, res) => { }
+
+const updateUser = async (req, res) => { }
+
+const deleteUser = (req, res) => { }
+
+module.exports = { getUser, updateUser, deleteUser }
