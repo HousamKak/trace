dfc86e575a4587d1f2f438a64db0d923391dc6a4
“Housam <housam.kak20@gmail.com>
2022-11-18 13:52:01 +0200

    backend => routed the saves APIs

--- backend/routes/saves.routes.js
+++ backend/routes/saves.routes.js
@@ -2,3 +2,7 @@ const { Router } = require('express');
 const router = Router();
 
 const { getAllSaves, addSave, deleteSave } = require('../controllers/saves.controller');
+
+router.get('/:user_id', getAllSaves);
+router.post('/', addSave);
+router.delete('/', deleteSave);
