3c239e17e6dea01a1137fa9a91c29f61653923c8
“Housam <housam.kak20@gmail.com>
2022-11-18 14:10:39 +0200

    backend => imported the getCloseTraces API

--- backend/routes/traces.routes.js
+++ backend/routes/traces.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const router = Router();
-const { getAllUserTraces, getTrace, deleteTrace, addTrace } = require('../controllers/traces.controller');
+const { getAllUserTraces, getTrace, deleteTrace, addTrace, getCloseTraces } = require('../controllers/traces.controller');
 
 router.get('/user/:user_id', getAllUserTraces);
 router.delete('/', deleteTrace);
