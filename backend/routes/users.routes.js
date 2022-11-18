f64868dc4eee078082c6a38321d4356759077be2
“Housam <housam.kak20@gmail.com>
2022-11-18 13:14:52 +0200

    backend => defined the routes for user functions

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,3 +1,7 @@
 const { Router } = require('express');
 const router = Router();
 const { getUser, updateUser, deleteUser } = require('../controllers/users.controller');
+
+router.get('/', getUser);
+router.put('/', updateUser);
+router.delete('/', deleteUser);
