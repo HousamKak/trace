799e11b130461e6f7203a789d8cc72e5e2b4edb1
“Housam <housam.kak20@gmail.com>
2022-11-18 23:33:22 +0200

    frontend => imported needed dependecies for Saved Page

---
+++ frontend/src/views/Menu/Saved.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import {
+  Text,
+  StyleSheet,
+  View,
+  ScrollView, RefreshControl
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import FullCard from "../../components/Cards/FullCard";
+import MenuBtn from "../../components/ButtonsMenu/MenuBtn";
+import AsyncStorage from "@react-native-async-storage/async-storage";
