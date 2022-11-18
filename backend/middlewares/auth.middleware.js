56e2432ff2c2bf0eecdc5789d4a83cc6dba5e6ed
“Housam <housam.kak20@gmail.com>
2022-11-18 13:17:59 +0200

    backend => verified token

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -3,7 +3,9 @@ const jwt = require('jsonwebtoken');
 
 const authMiddleware = async (req, res, next) => {
     const token = req.headers.authorization;
-    if (token) { }
+    if (token) {
+        const decoded = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET_KEY);
+    }
     else { res.json({ message: "Unauthorized" }) }
 }
 
