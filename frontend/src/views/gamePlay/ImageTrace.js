8ebac3a77faf228f49eaa7d721d327f9505d3359
“Housam <housam.kak20@gmail.com>
2022-11-19 17:17:07 +0200

    frontend => added the structure of ImageTrace

--- frontend/src/views/gamePlay/ImageTrace.js
+++ frontend/src/views/gamePlay/ImageTrace.js
@@ -3,7 +3,7 @@ import {
     Text,
     StyleSheet,
     View,
-    ScrollView, RefreshControl
+    ScrollView, RefreshControl, Image
 } from "react-native";
 import { useNavigation, } from "@react-navigation/native";
 import MenuBtn from "../../components/ButtonsMenu/MenuBtn";
@@ -13,6 +13,18 @@ const ImageTrace = () => {
     return (
         <ScrollView style={styles.ImageTracePage}>
             <View style={styles.header}>
+                <View>
+                    <View></View>
+                    <View></View>
+                </View>
+                <View>
+                    <Text ></Text>
+                    <Text></Text>
+                </View>
+
+            </View>
+            <View style={styles.ImageTrace}>
+                <Image source={""}></Image>
             </View>
             <View style={styles.footer}>
                 <MenuBtn src={require("../../assets/MenuPage/MenuButtons/closeIcon.png")} backgroundColor={styles.closeColor} onPress={() => navigation.navigate("MiddleButton")}></MenuBtn>
