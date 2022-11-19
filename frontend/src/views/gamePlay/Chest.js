20c7260b3b7f69d163ffb73656e54c55f28bc302
“Housam <housam.kak20@gmail.com>
2022-11-19 22:45:16 +0200

    frontend => added the structure of the Chest Page

--- frontend/src/views/gamePlay/Chest.js
+++ frontend/src/views/gamePlay/Chest.js
@@ -3,37 +3,37 @@ import {
     Text,
     StyleSheet,
     View,
-    ScrollView, RefreshControl
+    ScrollView, RefreshControl,Image
 } from "react-native";
 import { useNavigation, } from "@react-navigation/native";
 import MenuBtn from "../../components/ButtonsMenu/MenuBtn";
 import DataDisplay from "../../components/Displayers/DataDisplay";
 const Chest = () => {
     return (
-        <View>
-            <ScrollView>
-                <Image></Image>
+        <View style={styles.ChestPage}>
+            <ScrollView style={styles.ScrollView}>
+                <Image source={""} style={styles.chest}></Image>
                 <DataDisplay />
                 <View>
-
+                    {/* {scripts} */}
                 </View>
                 <DataDisplay />
                 <View>
-
+                    {/* {medals} */}
                 </View>
 
                 <View>
                     <View>
-                        <Image></Image>
-                        <Text></Text>
+                        <Image source={require("../../assets/MenuPage/MenuButtons/Gem.png")}></Image>
+                        <Text>3</Text>
                     </View>
                     <View>
-                        <Image></Image>
-                        <Text></Text>
+                        <Image source={require("../../assets/MenuPage/MenuButtons/Coin.png")}></Image>
+                        <Text>2000</Text>
                     </View>
                     <View>
-                        <Image></Image>
-                        <Text></Text>
+                        <Text>XP</Text>
+                        <Text>3000</Text>
                     </View>
                 </View>
             </ScrollView>
