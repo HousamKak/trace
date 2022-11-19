6450651a334d65ac3364362eb43ae9aea3ee6b2f
“Housam <housam.kak20@gmail.com>
2022-11-19 20:25:24 +0200

    frontend => added the closing button to Chest Page

--- frontend/src/views/gamePlay/Chest.js
+++ frontend/src/views/gamePlay/Chest.js
@@ -7,7 +7,17 @@ import {
 } from "react-native";
 import { useNavigation, } from "@react-navigation/native";
 
-const Chest = () => { };
+const Chest = () => {
+    return (
+        <View>
+            <ScrollView></ScrollView>
+            <View style={styles.footer}>
+                <MenuBtn src={require("../../assets/MenuPage/MenuButtons/closeIcon.png")} backgroundColor={styles.closeColor} onPress={() => navigation.navigate("MainPage")}></MenuBtn>
+            </View>
+        </View>
+
+    )
+};
 const styles = StyleSheet.create({});
 
 export default Chest;
