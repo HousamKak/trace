be834ef4aa59bb41b53549d2bb69c7b4fb4b7934
“Housam <housam.kak20@gmail.com>
2022-11-19 11:47:11 +0200

    frontend => structured and styled the component

--- frontend/src/components/Displayers/IconTypeDisplay.js
+++ frontend/src/components/Displayers/IconTypeDisplay.js
@@ -2,6 +2,10 @@ import * as React from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
 
 const IconTypeDisplay = (props) => {
+    return (
+        <View style={styles.ContDisplay}>
+            <Image style={styles.Icon} source={props.src} />
+        </View>)
 }
 
 const styles = StyleSheet.create({
