71a8d123da421ee88c6c2a2cfa63d4747525d0eb
“Housam <housam.kak20@gmail.com>
2022-11-19 11:50:31 +0200

    frontend => imported IconTypeDisplay to AddTrace

--- frontend/src/views/gamePlay/AddTrace.js
+++ frontend/src/views/gamePlay/AddTrace.js
@@ -6,6 +6,7 @@ import {
     ScrollView, RefreshControl
 } from "react-native";
 import { useNavigation, } from "@react-navigation/native";
+import IconTypeDisplay from "../../components/Displayers/IconTypeDisplay";
 
 const AddTrace = () => {
     <View style={styles.AddTracePage}>
@@ -15,15 +16,7 @@ const AddTrace = () => {
             source={require("../../assets/LogInPage/logo.png")}
         />
         <View styles={styles.IconTypeContainer}>
-            <View>
 
-            </View>
-            <View>
-
-            </View>
-            <View>
-
-            </View>
         </View>
     </View>
 };
