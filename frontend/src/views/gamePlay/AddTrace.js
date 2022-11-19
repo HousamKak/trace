ae4621264bf8ff5f2ca37d63b26d7aec6e716409
“Housam <housam.kak20@gmail.com>
2022-11-19 12:13:52 +0200

    frontend => added the return statement

--- frontend/src/views/gamePlay/AddTrace.js
+++ frontend/src/views/gamePlay/AddTrace.js
@@ -13,26 +13,27 @@ import MenuBtn from "../../components/ButtonsMenu/MenuBtn";
 
 const AddTrace = () => {
     const navigation = useNavigation();
-    <View style={styles.AddTracePage}>
-        <Image
-            style={styles.logoIcon}
-            resizeMode="cover"
-            source={require("../../assets/LogInPage/logo.png")}
-        />
-        <View styles={styles.IconTypeContainer}>
-            <IconTypeDisplay src={require("../../assets/MenuPage/TraceTypes/cameraIcon.png")} />
-            <IconTypeDisplay src={require("../../assets/MenuPage/TraceTypes/videoIcon.png")} />
-            <IconTypeDisplay src={require("../../assets/MenuPage/TraceTypes/musicIcon.png")} />
-        </View>
-        <View style={styles.textInputFields}>
-            <InputField placeholder="Add Title" />
-            <TextInput placeholder="Write Something" style={styles.multiline} multiline={true} maxLength={280} />
-            <SignBtn onPress={() => { navigation.navigate("MainPage") }} text="SIGN IN" cwidth={styles.sign} />
-        </View>
-        <View style={styles.footer}>
-            <MenuBtn src={require("../../assets/MenuPage/MenuButtons/closeIcon.png")} backgroundColor={styles.closeColor} onPress={() => navigation.navigate("MiddleButton")}></MenuBtn>
-        </View>
-    </View>
+    return (
+        <View style={styles.AddTracePage}>
+            <Image
+                style={styles.logoIcon}
+                resizeMode="cover"
+                source={require("../../assets/LogInPage/logo.png")}
+            />
+            <View styles={styles.IconTypeContainer}>
+                <IconTypeDisplay src={require("../../assets/MenuPage/TraceTypes/cameraIcon.png")} />
+                <IconTypeDisplay src={require("../../assets/MenuPage/TraceTypes/videoIcon.png")} />
+                <IconTypeDisplay src={require("../../assets/MenuPage/TraceTypes/musicIcon.png")} />
+            </View>
+            <View style={styles.textInputFields}>
+                <InputField placeholder="Add Title" />
+                <TextInput placeholder="Write Something" style={styles.multiline} multiline={true} maxLength={280} />
+                <SignBtn onPress={() => { navigation.navigate("MainPage") }} text="SIGN IN" cwidth={styles.sign} />
+            </View>
+            <View style={styles.footer}>
+                <MenuBtn src={require("../../assets/MenuPage/MenuButtons/closeIcon.png")} backgroundColor={styles.closeColor} onPress={() => navigation.navigate("MiddleButton")}></MenuBtn>
+            </View>
+        </View>)
 };
 
 const styles = StyleSheet.create({
