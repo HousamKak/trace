d0c860c8d4cab257d262d562dcb407b471793bc4
“Housam <housam.kak20@gmail.com>
2022-11-20 16:24:05 +0200

    frontend => added the script Images file

---
+++ frontend/src/utilities/Images/scriptImages.js
@@ -0,0 +1,43 @@
+export default itemImages = [
+    {
+        'title': 'phone',
+        'url': require("../../assets/MenuPage/Items/phone.png")
+    },
+    {
+        'title': 'pin',
+        'url': require("../../assets/MenuPage/Items/pin.png")
+    },
+    {
+        'title': 'safe',
+        'url': require("../../assets/MenuPage/Items/safe.png")
+    },
+    {
+        'title': 'weights',
+        'url': require("../../assets/MenuPage/Items/weights.png")
+    },
+    {
+
+        'title': 'ax',
+        'url': require("../../assets/MenuPage/Items/ax.png")
+    },
+    {
+        'title': 'book',
+        'url': require("../../assets/MenuPage/Items/book.png")
+    },
+    {
+        'title': 'creditCard',
+        'url': require("../../assets/MenuPage/Items/creditCard.png")
+    },
+    {
+        'title': 'direction',
+        'url': require("../../assets/MenuPage/Items/direction.png")
+    },
+    {
+        'title': 'headphones',
+        'url': require("../../assets/MenuPage/Items/headphones.png")
+    },
+    {
+        'title': 'key',
+        'url': require("../../assets/MenuPage/Items/key.png")
+    }
+]
