4a70a1a333c7b3640761dd6efa2bc93080d22ed6
“Housam <housam.kak20@gmail.com>
2022-11-20 16:28:28 +0200

    frontend => constructed the medal images exports file

---
+++ frontend/src/utilities/Images/medalImages.js
@@ -0,0 +1,43 @@
+export default scriptImages = [
+    {
+        'title': 'JapaneseStoneLantern',
+        'url': require("../../assets/MenuPage/Scripts/JapaneseStoneLantern.png")
+    },
+    {
+        'title': 'JapaneseTemple',
+        'url': require("../../assets/MenuPage/Scripts/JapaneseTemple.png")
+    },
+    {
+        'title': 'noResults',
+        'url': require("../../assets/MenuPage/Scripts/noResults.png")
+    },
+    {
+        'title': 'PalastinianFlag',
+        'url': require("../../assets/MenuPage/Scripts/PalastinianFlag.png")
+    },
+    {
+
+        'title': 'pumpkin',
+        'url': require("../../assets/MenuPage/Scripts/pumpkin.png")
+    },
+    {
+        'title': 'sandcastle',
+        'url': require("../../assets/MenuPage/Scripts/sandcastle.png")
+    },
+    {
+        'title': 'skull',
+        'url': require("../../assets/MenuPage/Scripts/skull.png")
+    },
+    {
+        'title': 'sword',
+        'url': require("../../assets/MenuPage/Scripts/sword.png")
+    },
+    {
+        'title': 'ToriiGate',
+        'url': require("../../assets/MenuPage/Scripts/ToriiGate.png")
+    },
+    {
+        'title': 'Water',
+        'url': require("../../assets/MenuPage/Scripts/Water.png")
+    }
+]
