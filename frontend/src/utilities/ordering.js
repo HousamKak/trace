7fd91ba87570bdf0771cf3b5268dbfa9f03a374e
“Housam <housam.kak20@gmail.com>
2022-11-20 15:35:31 +0200

    frontend => finalized the ordering function

--- frontend/src/utilities/ordering.js
+++ frontend/src/utilities/ordering.js
@@ -6,46 +6,48 @@ import {
     StyleSheet
 } from "react-native";
 
-const item = async (itemStorageKey, dividor, typePath) => {
+
+const GetImage = (title, IMAGES) => {
+    const found = IMAGES.find(e => e.title === title);
+    return found ? found.url : null;
+};
+
+const item = async (itemStorageKey, dividor, IMAGES) => {
     const items = await AsyncStorage.getItem(itemStorageKey)
     const itemsList = JSON.parse(items)
+    console.log(itemsList)
     if (itemsList) {
         const rapperArray = []
+        console.log(itemsList)
         if (Math.floor(itemsList.length / dividor) > 0) {
             for (var i = 0; i < Math.floor(itemsList.length / dividor); i++) {
                 const savedItem = itemsList.slice(dividor * i, dividor * (i + 1)).map((it) => {
-
-                    {
-
-                        let url = "../assets/MenuPage" + typePath + it.title + ".png"
-                        console.log(url)
-                        // it.url = require(url)
-
-                    }
-                    < Image key={it.item_id} source={it.url} />
+                    <Image key={it.item_id} source={GetImage(it.title, IMAGES)} style={styles.item} />
                 })
                 const rapper = <View style={styles.row}>{savedItem}</View>
                 rapperArray.push(rapper)
             }
-            const remainingItems = itemsList.slice(dividor * (i + 1)).map((it) => <Image key={it.item_id} source={require("../assets/MenuPage/MenuButtons/friendsIcon.png")} style={styles.item} />)
+            const remainingItems = itemsList.slice(4 * (i + 1)).map((it) => <Image key={it.item_id} source={GetImage(it.title, IMAGES)} style={styles.item} />)
             const rapper = <View style={styles.lastRow}>{remainingItems}</View>
             rapperArray.push(rapper)
             const savedItems = rapperArray.map((rap) => rap)
             setMyItems(savedItems)
         }
         else {
-            const savedItem = itemsList.map((it) => <Image key={it.item_id} source={require("../assets/MenuPage/MenuButtons/friendsIcon.png")} style={styles.aloneitem} />)
+            const savedItem = itemsList.map((it) => <Image key={it.item_id} source={GetImage(it.title, IMAGES)} style={styles.aloneitem} />)
             const rapper = <View style={styles.lastRow}>{savedItem}</View>
             rapperArray.push(rapper)
             const savedItems = rapperArray.map((rap) => rap)
             setMyItems(savedItems)
         }
+        console.log(rapperArray)
     }
     else {
         const savedItems = <Text style={styles.noContent}>No Saved Items</Text>
         setSaves(savedItems)
     }
 }
+
 const styles = StyleSheet.create({
     row: {
         flexDirection: "row",
