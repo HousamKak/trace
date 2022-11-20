bcd56013b9bb74cb89429191753568b4e13ff773
“Housam <housam.kak20@gmail.com>
2022-11-20 17:01:31 +0200

    backend => edited the id name of items medals scripts

--- backend/controllers/items.controller.js
+++ backend/controllers/items.controller.js
@@ -3,7 +3,7 @@ const db = require('../config/db.config')
 const getAllItems = (req, res) => {
     const { user_id } = req.params;
     if (user_id) {
-        db.query('SELECT * FROM items WHERE item_id IN(select item_id from chests_has_items where chest_id IN(select chest_id from users_has_chests where user_id = ?))', [user_id], (err, rows) => {
+        db.query('SELECT * FROM items WHERE id IN(select item_id from chests_has_items where chest_id IN(select chest_id from users_has_chests where user_id = ?))', [user_id], (err, rows) => {
             if (err) console.log(err);
             res.send(rows);
         })
--- backend/controllers/medals.controller.js
+++ backend/controllers/medals.controller.js
@@ -2,7 +2,7 @@ const db = require('../config/db.config')
 const getAllMedals = (req, res) => {
     const { user_id } = req.params;
     if (user_id) {
-        db.query('SELECT * FROM medals WHERE medal_id IN(select medal_id from chests_has_medals where chest_id IN(select chest_id from users_has_chests where user_id = ?))', [user_id], (err, rows) => {
+        db.query('SELECT * FROM medals WHERE id IN(select medal_id from chests_has_medals where chest_id IN(select chest_id from users_has_chests where user_id = ?))', [user_id], (err, rows) => {
             if (err) console.log(err);
             res.send(rows);
         })
