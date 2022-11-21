afcd97419e20e3d2f6159639325b1f72b57bba68
“Housam <housam.kak20@gmail.com>
2022-11-21 02:19:40 +0200

    backend => increased the limit of request

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,9 @@ const fs = require('fs');
 require('dotenv').config();
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+// app.use(express.urlencoded({ limit: '50mb' }));
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
