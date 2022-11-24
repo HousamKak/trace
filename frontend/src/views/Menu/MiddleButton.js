67a53cfb48b8f001c2a9a502b40b48014e463e35
“Housam <housam.kak20@gmail.com>
2022-11-24 15:09:37 +0200

    frontend => removed base url from middle button

--- frontend/src/views/Menu/MiddleButton.js
+++ frontend/src/views/Menu/MiddleButton.js
@@ -11,7 +11,7 @@ import MenuBtnText from "../../components/ButtonsMenu/MenuBtnText";
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { useNavigation } from "@react-navigation/native";
 import { getData } from "../../utilities/axios/getData";
-const base_url = "http://192.168.1.102:8000"
+
 
 
 const MiddleButton = () => {
