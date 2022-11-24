054cb43ce5417a6f47fc6b9d200a1b86e147957c
“Housam <housam.kak20@gmail.com>
2022-11-24 15:06:17 +0200

    frontend => removed the getData from MiddleButton

--- frontend/src/views/Menu/MiddleButton.js
+++ frontend/src/views/Menu/MiddleButton.js
@@ -19,23 +19,6 @@ const MiddleButton = () => {
     const navigation = useNavigation();
     const [loading, setLoading] = React.useState(false);
 
-    const getData = async (route, key) => {
-        const user_prime = await AsyncStorage.getItem("user")
-        const user = JSON.parse(user_prime)
-        const configurationObject = {
-            method: "GET",
-            url: base_url + route + user.user_id,
-            headers: {
-                "Access-Control-Allow-Origin": "*",
-            },
-        }
-        try {
-            const response = await axios(configurationObject)
-            if (response.status === 200) {
-                AsyncStorage.setItem(key, JSON.stringify(response.data))
-            }
-        } catch (e) { console.log(e.message) }
-    }
     return (
         <ScrollView style={styles.middleButtonView}>
             <View style={styles.header}>
