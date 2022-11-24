43d564b100d37242e0724628ddbcc99df7869d3b
“Housam <housam.kak20@gmail.com>
2022-11-24 17:03:10 +0200

    backend => edited the getUserChests route

--- backend/routes/chests.routes.js
+++ backend/routes/chests.routes.js
@@ -1,9 +1,9 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getChest, getCloseChests, getUserChests} = require('../controllers/chests.controller');
+const { getChest, getCloseChests, getUserChests } = require('../controllers/chests.controller');
 
-router.get('/:user_id', getChest);
+router.get('/user/:user_id', getUserChests);
 router.get('/:chest_id', getChest);
 router.get('/', getCloseChests);
 
